2561e8a065b7039ed8461957945fb528643cf29d
Guilherme Rodrigues <guilher.c.rodrigues@gmail.com>
2024-02-27 20:16:16 -0300

    feat. basic routing package

---
+++ router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+  "github.com/gin-gonic/gin"
+  "net/http"
+)
+
+func Initialize () {
+    r := gin.Default()
+
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H {
+            "message":"BAW!",
+        })
+    })
+    r.Run()
+
+}
